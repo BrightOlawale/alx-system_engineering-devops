43b85c12086001aaf7b74ccd4cf4456be7661275
Bright Olawale <oolatunjibright@yahoo.com>
2022-11-09 04:31:52 +0100

    Export to JSON

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,22 +1,33 @@
 #!/usr/bin/python3
 """
-a script that returns info about a customer's todo list
+Module 0-gather_data_from_an_API
 """
+
 import json
 import requests
 from sys import argv
 
-
 if __name__ == '__main__':
-    url = "https://jsonplaceholder.typicode.com"
-    employeeId = argv[1]
 
-    employee = requests.get("{}/users/{}".format(url, employeeId)).json()
-    todos = requests.get(url + "/todos", params={"userId": employeeId}).json()
+    id = argv[1]
+    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(
+        id)).json()
+    username = user.get('username')
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
+                        format(id)).json()
+    json_filename = id + ".json"
 
-    userName = employee.get('username')
-    fileName = employeeId + ".json"
+    tasks = []
 
-    line = []
-    for info in todos:
-        info.append({'task': info.get('title'),  })
+    for task in todo:
+        task_dict = {}
+        task_dict["task"] = task.get('title')
+        task_dict["completed"] = task.get('completed')
+        task_dict["username"] = username
+        tasks.append(task_dict)
+
+    dictionary = {}
+    dictionary[id] = tasks
+
+    with open(json_filename, "w") as json_file:
+        json.dump(dictionary, json_file)
